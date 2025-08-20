ee45799c89c5347d3b439e6d0607e721997bf473
George Ajileye <georgeajileye1@gmail.com>
2025-08-20 07:52:06 -0400

    Updates

--- src/component/styleLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Children } from 'react'
-import '../index.css'
-export default function StyleLayout({children}) {
-  return (
-    <div className="styleLayout">
-        {children}
-    </div>
-  )
-}
